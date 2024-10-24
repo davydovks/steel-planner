0892ee80c10f9ab929a56fbef6ed414b190e41f5
Konstantin Davydov <davydovks@mail.ru>
2024-10-24 19:32:13 +0500

    write part.edit view

--- resources/js/Pages/Parts/Edit.vue
+++ resources/js/Pages/Parts/Edit.vue
@@ -1,24 +1,22 @@
 <template>
   <div>
-    <Head :title="`${form.name}`" />
+    <Head :title="`Деталь ${form.position}`" />
     <h1 class="mb-8 text-3xl font-bold">
-      <Link class="text-indigo-400 hover:text-indigo-600" href="/structures">Отправочные марки</Link>
+      <Link class="text-indigo-400 hover:text-indigo-600" href="/parts">Детали</Link>
       <icon name="cheveron-right" class="inline w-6 h-6 ml-1 fill-gray-400" />
-      <span>{{ form.name }} (редактирование)</span>
+      <span>{{ form.position }} (редактирование)</span>
     </h1>
-    <trashed-message v-if="structure.deleted_at" class="mb-6" @restore="restore">Эта отправочная марка удалена.</trashed-message>
+    <trashed-message v-if="part.deleted_at" class="mb-6" @restore="restore">Эта деталь удалена.</trashed-message>
     <div class="max-w-3xl bg-white rounded-md shadow overflow-hidden">
       <form @submit.prevent="update">
         <div class="flex flex-wrap -mb-8 -mr-6 p-8">
-          <text-input v-model="form.name" :error="form.errors.name" class="pb-8 pr-6 w-full lg:w-1/2" label="Название" />
-          <text-input v-model="form.quantity" v-maska="options" data-maska-number data-maska-number-locale="ru" :error="form.errors.quantity" class="pb-8 pr-6 w-full lg:w-1/2" label="Количество" />
-          <!-- <select-input v-model="form.order_id" :error="form.errors.order_id" class="pb-8 pr-6 w-full lg:w-1/2" label="Заказ">
-            <option v-for="order in orders" :key="order.id" :value="order.id">{{ order.name }}</option>
-          </select-input> -->
-          <text-div v-model="form.order_name" class="pb-8 pr-6 w-full" label="Заказ" :link="form.order_link" />
+          <text-div v-model="form.order_name" class="pb-8 pr-6 w-full lg:w-1/2" label="Заказ" :link="form.order_link" />
+          <text-input v-model="form.position" v-maska="options" :error="form.errors.position" class="pb-8 pr-6 w-full lg:w-1/2" label="Позиция" />
+          <text-input v-model="form.profile" :error="form.errors.profile" class="pb-8 pr-6 w-full lg:w-1/2" label="Профиль" />
+          <text-input v-model="form.weight" data-maska-number data-maska-number-locale="ru" :error="form.errors.position" class="pb-8 pr-6 w-full lg:w-1/2" label="Вес детали, кг" />
         </div>
         <div class="flex items-center px-8 py-4 bg-gray-50 border-t border-gray-100">
-          <Link class="btn-default" :href="`/structures/${structure.id}`">Отмена</Link>
+          <Link class="btn-default" :href="`/parts/${part.id}`">Отмена</Link>
           <loading-button :loading="form.processing" class="btn-indigo ml-auto" type="submit">Сохранить</loading-button>
         </div>
       </form>
@@ -33,7 +31,6 @@ import Icon from '@/Shared/Icon.vue'
 import Layout from '@/Shared/Layout.vue'
 import TextDiv from '@/Shared/TextDiv.vue'
 import TextInput from '@/Shared/TextInput.vue'
-// import SelectInput from '@/Shared/SelectInput.vue'
 import LoadingButton from '@/Shared/LoadingButton.vue'
 import TrashedMessage from '@/Shared/TrashedMessage.vue'
 
@@ -43,7 +40,6 @@ export default {
     Icon,
     Link,
     LoadingButton,
-    // SelectInput,
     TextDiv,
     TextInput,
     TrashedMessage,
@@ -51,19 +47,17 @@ export default {
   directives: { maska: vMaska },
   layout: Layout,
   props: {
-    structure: Object,
-    // orders: Array,
+    part: Object,
   },
   remember: 'form',
   data() {
     return {
       form: this.$inertia.form({
-        name: this.structure.name,
-        quantity: String(this.structure.quantity),
-        // order_id: this.structure.order.id,
-        order_link: `/orders/${this.structure.order.id}`,
-        order_name: this.structure.order.name,
-        percent: "0",
+        order_link: `/orders/${this.part.order.id}`,
+        order_name: this.part.order.name,
+        position: String(this.part.position),
+        profile: this.part.profile,
+        weight: (this.part.weight / 1000).toFixed(1),
       }),
       options: {
         mask: "#*",
@@ -76,11 +70,11 @@ export default {
   },
   methods: {
     update() {
-      this.form.put(`/structures/${this.structure.id}`)
+      this.form.put(`/parts/${this.part.id}`)
     },
     restore() {
-      if (confirm('Вы уверены, что хотите восстановить этот заказ?')) {
-        this.$inertia.put(`/structures/${this.structure.id}/restore`)
+      if (confirm('Вы уверены, что хотите восстановить эту деталь?')) {
+        this.$inertia.put(`/parts/${this.part.id}/restore`)
       }
     },
   },
